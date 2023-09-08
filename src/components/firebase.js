5e2518602c77bcbda0163a930a118a93b5a2bc12
soum-ik <sarkarsoumik215@gmail.com>
2023-09-09 01:12:23 +0600

    env file

--- src/components/firebase.js
+++ src/components/firebase.js
@@ -1,15 +1,14 @@
 import { initializeApp } from "firebase/app";
 
 const app = initializeApp({
-  apiKey: "AIzaSyCmyD6LkC4I3NaPY6E7gJOsX4h0zmigtyc",
-  authDomain: "developer-9o9.firebaseapp.com",
-  projectId: "developer-9o9",
-  storageBucket: "developer-9o9.appspot.com",
-  messagingSenderId: "563748231896",
-  appId: "1:563748231896:web:2ab6fb59704daae9e0d5ad",
-  measurementId: "G-1DM9XXLYEP",
-  databaseURL:
-    "https://developer-9o9-default-rtdb.asia-southeast1.firebasedatabase.app",
+  apiKey: import.meta.env.VITE_REACT_API_KEY,
+  authDomain: import.meta.env.VITE_REACT_AUTH_DOMIN,
+  projectId: import.meta.env.VITE_REACT_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_REACT_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_REACT_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_REACT_APP_ID,
+  measurementId: import.meta.env.VITE_REACT_MEASUREMENT_ID,
+  databaseURL: import.meta.env.VITE_REACT_DATABASE_URL,
 });
 
 export default app;
