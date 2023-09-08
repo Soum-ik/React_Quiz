054545e1cda144d846df187d8e97fb7114fb9710
soum-ik <sarkarsoumik215@gmail.com>
2023-09-09 03:18:51 +0600

    css

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -1,7 +1,8 @@
-import Logo from "../assets/images/logo-bg.png";
+import Logo from "../assets/images/logo.png";
 import classes from "../styles/Nav.module.css";
 import Account from "./account";
 import { Link } from "react-router-dom";
+import "../styles/app.css"
 
 export default function Nav() {
   return (
@@ -9,8 +10,8 @@ export default function Nav() {
       <ul>
         <li>
           <Link to="/" className={classes.brand}>
-            <img src={Logo} alt="Learn with Sumit Logo" />
-            <h3>Learn with Soumik</h3>
+            <img src={Logo} alt="Learn with Sumit Logo"/>
+            <h3>Quiz</h3>
           </Link>
         </li>
       </ul>
